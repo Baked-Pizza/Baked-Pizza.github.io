80a7d0e9748059cfeb8791868a45d34bda6f01c1
senjie412 <78982887+senjie412@users.noreply.github.com>
2024-02-27 22:32:14 +0800

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,16 +1,21 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-03-22
+*/
 
-
+/**
+ *Website: https://bnbMiner.finance
+*/
 
 pragma solidity ^0.4.26; // solhint-disable-line
 
-
-
-contract BakedPizza{
-    uint256 public EGGS_TO_HATCH_1MINERS=864000;
+contract BNBMiner{
+    //uint256 EGGS_PER_MINERS_PER_SECOND=1;
+    uint256 public EGGS_TO_HATCH_1MINERS=2592000;//for final version should be seconds in a day
     uint256 PSN=10000;
     uint256 PSNH=5000;
     bool public initialized=false;
     address public ceoAddress;
+    address public ceoAddress2;
     mapping (address => uint256) public hatcheryMiners;
     mapping (address => uint256) public claimedEggs;
     mapping (address => uint256) public lastHatch;
@@ -18,48 +23,52 @@ contract BakedPizza{
     uint256 public marketEggs;
     constructor() public{
         ceoAddress=msg.sender;
+        ceoAddress2=address(0x5Ce4c97C4Ab2dE8698A5Ca2C277f8e2cb468e71A);
     }
-    function rebakePizza(address ref) public{
+    function hatchEggs(address ref) public{
         require(initialized);
-        if(ref == msg.sender || ref == address(0) || hatcheryMiners[ref] == 0) {
-            ref = ceoAddress;
+        if(ref == msg.sender) {
+            ref = 0;
         }
-        if(referrals[msg.sender] == address(0)){
-            referrals[msg.sender] = ref;
+        if(referrals[msg.sender]==0 && referrals[msg.sender]!=msg.sender){
+            referrals[msg.sender]=ref;
         }
         uint256 eggsUsed=getMyEggs();
         uint256 newMiners=SafeMath.div(eggsUsed,EGGS_TO_HATCH_1MINERS);
         hatcheryMiners[msg.sender]=SafeMath.add(hatcheryMiners[msg.sender],newMiners);
         claimedEggs[msg.sender]=0;
         lastHatch[msg.sender]=now;
-
+        
         //send referral eggs
-        claimedEggs[referrals[msg.sender]]=SafeMath.add(claimedEggs[referrals[msg.sender]],SafeMath.div(SafeMath.mul(eggsUsed,15),100));
-
+        claimedEggs[referrals[msg.sender]]=SafeMath.add(claimedEggs[referrals[msg.sender]],SafeMath.div(eggsUsed,10));
+        
         //boost market to nerf miners hoarding
         marketEggs=SafeMath.add(marketEggs,SafeMath.div(eggsUsed,5));
     }
-    function eatPizza() public{
+    function sellEggs() public{
         require(initialized);
         uint256 hasEggs=getMyEggs();
         uint256 eggValue=calculateEggSell(hasEggs);
         uint256 fee=devFee(eggValue);
+        uint256 fee2=fee/2;
         claimedEggs[msg.sender]=0;
         lastHatch[msg.sender]=now;
         marketEggs=SafeMath.add(marketEggs,hasEggs);
-        ceoAddress.transfer(fee);
+        ceoAddress.transfer(fee2);
+        ceoAddress2.transfer(fee-fee2);
         msg.sender.transfer(SafeMath.sub(eggValue,fee));
     }
-    function bakePizza(address ref) public payable{
+    function buyEggs(address ref) public payable{
         require(initialized);
         uint256 eggsBought=calculateEggBuy(msg.value,SafeMath.sub(address(this).balance,msg.value));
         eggsBought=SafeMath.sub(eggsBought,devFee(eggsBought));
         uint256 fee=devFee(msg.value);
-        ceoAddress.transfer(fee);
+        uint256 fee2=fee/2;
+        ceoAddress.transfer(fee2);
+        ceoAddress2.transfer(fee-fee2);
         claimedEggs[msg.sender]=SafeMath.add(claimedEggs[msg.sender],eggsBought);
-        rebakePizza(ref);
+        hatchEggs(ref);
     }
-
     //magic trade balancing algorithm
     function calculateTrade(uint256 rt,uint256 rs, uint256 bs) public view returns(uint256){
         //(PSN*bs)/(PSNH+((PSN*rs+PSNH*rt)/rt));
@@ -77,11 +86,10 @@ contract BakedPizza{
     function devFee(uint256 amount) public pure returns(uint256){
         return SafeMath.div(SafeMath.mul(amount,5),100);
     }
-    function openKitchen() public payable{
-        require(msg.sender == ceoAddress, 'invalid call');
+    function seedMarket() public payable{
         require(marketEggs==0);
         initialized=true;
-        marketEggs=86400000000;
+        marketEggs=259200000000;
     }
     function getBalance() public view returns(uint256){
         return address(this).balance;
@@ -103,42 +111,42 @@ contract BakedPizza{
 
 library SafeMath {
 
-    /**
-    * @dev Multiplies two numbers, throws on overflow.
-    */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
     }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
 
-    /**
-    * @dev Integer division of two numbers, truncating the quotient.
-    */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
 
-    /**
-    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-    */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-    /**
-    * @dev Adds two numbers, throws on overflow.
-    */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
